80d934cafe28c965c0dd90674412c4cb54fb6b04
masa <iwamasa.0526.to@gmail.com>
2020-12-26 21:04:35 +0900

    ページ遷移機能完了

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-function App() {
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { About } from "./pages/About";
+import { Cat } from "./pages/Cat";
+import { Top } from "./pages/Top";
+import { Portfolio } from "./pages/Portfolio";
+import { Signin } from "./pages/Signin";
+
+const App = () => {
   return (
-    <div className="App">
-      <h1>Appページ</h1>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Top} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/cat" component={Cat} />
+        <Route exact path="/portfolio" component={Portfolio} />
+        <Route exact path="/signin" component={Signin} />
+      </Switch>
+    </Router>
   );
-}
+};
 
 export default App;
